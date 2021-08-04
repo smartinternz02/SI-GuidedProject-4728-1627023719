539d46e008d8380481b7d305a979cb74aeaa4ab1
Medha-Vyshnavi <88196010+Medha-Vyshnavi@users.noreply.github.com>
2021-08-04 13:07:52 +0530

    Delete main (1).js

--- main (1).js
+++
@@ -1,6 +0,0 @@
-document.getElementById('imageUpload').onchange = function(event){
-    console.log('Ínside upload Image');
-    var image = document.getElementById("img-preview");
-    image.src = URL.createObjectURL(event.target.files[0]);
-    image.style.display = 'block';
-}
